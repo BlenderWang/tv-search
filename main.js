fa2dc8ab50ecba735cc6bd5e17a6fb1c7c4e8127
Shirley Wang <shirleywang527@gmail.com>
2019-04-27 08:00:00 +0200

    get rid of one concole error regarding fetching data json

--- main.js
+++ main.js
@@ -5,13 +5,15 @@ searchBtn.addEventListener('click', (e) => {
     e.preventDefault()
 
     const searchField = document.querySelector('.search-field').value
-    const url = 'http://api.tvmaze.com/singlesearch/shows?q=' + searchField
+    const url = 'http://api.tvmaze.com/singlesearch/shows?q=' + searchField + '&embed=episodes'
 
     const myRequest = new XMLHttpRequest()
-    myRequest.open('GET', url)
-    myRequest.onload = function() {
-        const myData = JSON.parse(myRequest.responseText)
-        renderHTML(myData)
+    myRequest.open('GET', url, true)
+    myRequest.onreadystatechange = function() {
+        if(myRequest.readyState === 4 && myRequest.status == 200) {
+            const myData = JSON.parse(myRequest.responseText)
+            renderHTML(myData)
+        }
     }
     myRequest.send()
 })
@@ -23,7 +25,23 @@ function renderHTML(data) {
     output.innerHTML = `
         <img src="${data.image.original}" alt="">
         <h2 class="name">${data.name}</h2>
+        <p class="rating">Rating: <span>${data.rating.average}</span></p>
         <div class="summary">${data.summary}</div>
+        <div class="season-list">
+            <button><a href="http://api.tvmaze.com/shows/1/seasons">S 1</a></button>
+            <button><a href="http://api.tvmaze.com/shows/2/seasons">S 2</a></button>
+            <button><a href="http://api.tvmaze.com/shows/3/seasons">S 3</a></button>
+        </div>
+        <div class="episode-list">
+            <div class="left">
+                <h5>S1 Ep2</h5>
+                <img src="http://static.tvmaze.com/uploads/images/medium_landscape/31/79061.jpg" alt="">
+            </div>
+            <div class="right">
+                <h4 class="name">Fist Like a Bullet</h4>
+                <p class="name">M.K. finds refuge in an unlikely and dangerous place, while Sunny's loyalty is tested when Quinn tries to force him to commit an unspeakable act.</p>
+            </div>
+        </div>
     `
     resDisplay.appendChild(output)
 }
